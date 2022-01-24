0e3bfa9f8db3b43d11543b781087835c5531bb75
Yqylas <y.saiyn@yandex.kz>
2022-01-24 16:02:51 +0600

    Up

---
+++ first.py
@@ -0,0 +1,21 @@
+frst = int(input('введите первое число: '))
+print(frst)
+scnd = input('что будем делать: сложить (+), вычесть (-), умножить(*), делить(/):  ')
+print(scnd)
+thrd = int(input('введите второе число'))
+while True:
+    if scnd == '+':
+        print(frst + thrd);
+    elif scnd == '-':
+        print(frst - thrd);
+    elif scnd == '*':
+        print(frst * thrd);
+    elif scnd == '/' and thrd != 0:
+        print(frst / thrd);
+    elif scnd == '/' and thrd == 0:
+        print('на ноль делить нельзя');
+    else:
+        print('неизвестная команда');
+    break
+
+        
